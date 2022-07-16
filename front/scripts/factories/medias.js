cd470b855dd77ca5aa21fce47bdcf7289273941a
Seb <79709673+SebDevYogi@users.noreply.github.com>
2022-07-16 22:26:41 +0200

    fonctions affichages pour les vidéos, compteurs de like par photographe, animation des coeurs, fonctions d'ajout/suppression de like

--- front/scripts/factories/medias.js
+++ front/scripts/factories/medias.js
@@ -1,9 +1,99 @@
-function photographerPage() {
-    function photographerPageHeader() {
-    }
-    
-    function photographerPageGallery() {
+    function createTemplate () {
+        const orderBar = document.createElement("div");
+        orderBar.setAttribute("class", "order_bar");
+        orderBar.innerHTML = `
+            <p>Trier par </p>
+            <nav class="dropdown">
+                <ul>Popularité <i class="fa-solid fa-angle-up"></i>
+                    <li>Date</li>
+                    <li>Likes</li>
+                </ul>
+            </nav>
+        `;        
+        const mediasGallery = document.createElement("div");
+        mediasGallery.setAttribute("class", "medias_gallery");
+        mediasGallery.innerHTML = `
+        <div class="floating_bar">
+            <div id="like_total">
+            <p id="like_amount">0</p>
+            <i class="fas fa-heart fa_full fa_full_animation"></i>
+            </div>
+            <div id="rate">
+            </div>
+        </div>
+            `;
 
+
+        gallery.appendChild(orderBar);
+        gallery.appendChild(mediasGallery);
     }
-    return photographerPageHeader, photographerPageGallery;
-}
+
+    function loadImages (array, parent) {
+        const card = document.createElement("div");
+        card.setAttribute("class", "card");
+        card.innerHTML = `
+            <img src="../front/assets/images/${array.photographerId}/${array.image}" alt="${array.title}">
+            <div class="card_info">
+                <h3>${array.title}</h3>
+                <span>
+                    <p class="art_likes">${array.likes}</p>
+                    <i id="custom_heart_${array.image}" class="far fa-heart fa_custom"></i>
+                </span>
+            </div>
+        `;
+        parent.appendChild(card);
+    }
+
+    function loadVideos (array, parent) {
+        const card = document.createElement("div");
+        card.setAttribute("class", "card");
+        card.innerHTML = `
+            <video>
+                <i class="fa-solid fa-circle-play"></i>
+                <source src="../front/assets/images/${array.photographerId}/${array.video}" type="video/mp4">
+            </video>
+            <div class="card_info">
+                <h3>${array.title}</h3>
+                <span>
+                    <p class="art_likes">${array.likes}</p>
+                    <i class="far fa-heart fa_custom"></i>
+                </span>
+            </div>
+        `;
+        parent.appendChild(card);   
+    }
+
+    function totalLikes () {
+        const hearts = document.querySelectorAll(".art_likes");
+        let total = 0;
+        hearts.forEach(heart => {
+            total += parseInt(heart.textContent);
+        });
+        document.querySelector("#like_amount").innerHTML = total;
+    }
+
+    function likeHearts () {
+        const hearts = document.querySelectorAll(".fa_custom");
+        hearts.forEach(heart => {
+            heart.addEventListener("click", function () {
+                const likeNb = heart.previousElementSibling.textContent;        
+                if (heart.classList.contains("fa_custom")) {
+                    heart.previousElementSibling.innerHTML = parseInt(likeNb)+1;
+                    heart.classList.remove("fa_custom");
+                    heart.classList.remove("far");
+                    heart.classList.add("fas");
+                    heart.classList.add("fa_full");
+                    heart.classList.add("fa_full_animation");
+                    totalLikes();
+                } else {
+                    heart.previousElementSibling.innerHTML = parseInt(likeNb)-1;
+                    heart.classList.remove("fa_full_animation")
+                    heart.classList.remove("fa_full");
+                    heart.classList.remove("fas");
+                    heart.classList.add("far");
+                    heart.classList.add("fa_custom");
+                    totalLikes();
+                }
+            });
+        });
+    }
--- front/scripts/pages/photographer.js
+++ front/scripts/pages/photographer.js
@@ -12,10 +12,10 @@
         return { photographers };
     }
 
+
     async function displayHeader(data) {
         const photographersId = window.location.href.substring(window.location.href.indexOf("=") + 1);
         const photographer = data.photographers.find(photographer => photographer.id == photographersId);
-        console.log(photographer);
 
         const photograhHeader = document.querySelector(".photograph-header");
         const photographerInfo = document.createElement("div");
@@ -33,51 +33,46 @@
         photograhHeader.appendChild(photographerProfilPicture);
     }
 
+
     async function displayGallery(data) {
         const photographersId = window.location.href.substring(window.location.href.indexOf("=") + 1);
+        const photographer = data.photographers.find(photographer => photographer.id == photographersId);
 
-        const orderBar = document.createElement("div");
-        orderBar.setAttribute("class", "order_bar");
-        const orderText = document.createElement("p");
-        orderText.innerHTML = "Trier par "; 
-        const dropdown = document.createElement("nav");
-        dropdown.innerHTML = `
-            <button>Popularité <i class="fa-solid fa-angle-up"></i></button>`;
-        dropdown.setAttribute("class", "dropdown");
-        const mediasGallery = document.createElement("div");
-        mediasGallery.setAttribute("class", "medias_gallery");
+        createTemplate();
 
-        gallery.appendChild(orderBar);
-        gallery.appendChild(mediasGallery);
-        orderBar.appendChild(orderText);
-        orderBar.appendChild(dropdown);
+        document.getElementById("rate").innerHTML = `
+            ${photographer.price}€ / jour
+        `;
+
+
+        let mediasGallery = document.querySelector(".medias_gallery");
 
         for (let i = 0; i < data.media.length; i++) {
             if (data.media[i].photographerId == photographersId) {
                 let medias = [];
                 medias.push(data.media[i]);
                 for (let j = 0; j < medias.length; j++) {
-                    const card = document.createElement("div");
-                    card.setAttribute("class", "card");
-                    card.innerHTML = `
-                        <img src="../front/assets/images/${medias[j].photographerId}/${medias[j].image}" alt="${medias[j].title}">
-                        <div class="card_info">
-                            <h3>${medias[j].title}</h3>
-                            <span>${medias[j].likes} <i class="fa-solid fa-heart"></i></span>
-                        </div>
-                    `;
-                    mediasGallery.appendChild(card);
-                }
+                    if (medias[j].video === undefined) {
+                        loadImages(medias[j], mediasGallery);
+                    } else {
+                        loadVideos(medias[j], mediasGallery);
+                    };
+                };
             }
         }
     }
 
+
     async function init() {
         // Récupère les datas des photographes
         const { photographers } = await getPhotographersDatas();
-        console.log(photographers);
         displayHeader(photographers);
         displayGallery(photographers);
+        totalLikes();
+        likeHearts();
     };
-    
+
     init();
+
+
+    
